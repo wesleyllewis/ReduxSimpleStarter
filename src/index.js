d0e5b8874acb94ea2694645120a7514bcf1ebf19
wesleyllewis <reverendenoch@gmail.com>
2017-05-10 16:21:44 -0500

    Finished Initial Set Up of DOM

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a new Component. this component should produce some HTML
-const App = function() {
+const App = () => {
   return <div> Hi! </div>;
 }
 // Take this component's generated HTML and put it on the page(in the DOM)
