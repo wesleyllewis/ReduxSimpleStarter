117d86f6b3d05d7ba371a6988ebb2e6e023c2ae3
wesleyllewis <reverendenoch@gmail.com>
2017-05-12 13:13:07 -0500

    Added bare components, Search Bar Configured, End of Section 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Create a new Component. this component should produce some HTML
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyC-f3jyjt2Y1QJRY4AidVcUqOIi2-xtnBk';
+
+// Create a new Component. this component should produce
+//some HTML
 const App = () => {
-  return <div> Hi! </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
-// Take this component's generated HTML and put it on the page(in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+
+// Take this component's generated HTML and put it 
+//on the page(in the DOM)
+ReactDOM.render(<App /> , document.querySelector('.container'));
